1f549c01b710488a9d5e5efef57f8a7135d1e0cb
Diego Ferigo <dgferigo@gmail.com>
2022-05-26 07:43:39 +0200

    Fix infinite loop for cycles in pytrees (#2)
    * Fix infinite loop for deeply nested pytrees
    
    * Update creation of optional argument
    
    * Refactor to more compact logic
    
    * Minor style tweak
    
    * Fix style with black
    
    * Tweaks, tests for cycle detection
    
    * Bump version
    
    Co-authored-by: Brent Yi <yibrenth@gmail.com>

---
+++ tests/test_cycle.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import Tuple
+
+from jax import numpy as jnp
+
+import jax_dataclasses as jdc
+
+
+def test_cycle():
+    @jdc.pytree_dataclass
+    class TreeNode:
+        content: jnp.ndarray
+        children: Tuple[TreeNode, ...]
+
+    a = TreeNode(content=jnp.zeros(3), children=())
+    b = TreeNode(content=jnp.ones(3), children=(a,))
+
+    # Not a cycle. OK!
+    with jdc.copy_and_mutate(b, validate=False) as b:
+        b.children = (a, a)
+
+    # Cycle. Ideally this should raise an error, but the way duplicate nodes in pytrees
+    # are possible makes robust cycle detection under linear space/time constraints a
+    # hassle. So instead we currently do nothing.
+    with jdc.copy_and_mutate(b, validate=False) as b:
+        b.children[0].children = (b,)
