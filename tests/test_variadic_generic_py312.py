c1fdb8d6269c438ba5847eb04bd80c57184c412d
Brent Yi <yibrenth@gmail.com>
2024-07-02 15:55:26 +0900

    Generics tests

---
+++ tests/test_variadic_generic_py312.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Generic, TypeVarTuple
+
+import jax_dataclasses as jdc
+
+Ts = TypeVarTuple("Ts")
+
+
+@jdc.pytree_dataclass
+class Args(Generic[*Ts]):
+    @staticmethod
+    @jdc.jit
+    def make(args: jdc.Static[tuple[*Ts]]) -> tuple[Args[*Ts], tuple[*Ts]]:
+        return Args(), args
+
+
+def test0() -> None:
+    assert Args.make((1, 2, 3))[1] == (1, 2, 3)
+
+
+@jdc.pytree_dataclass
+class Args2[*T]:
+    @staticmethod
+    @jdc.jit
+    def make[*T_](args: jdc.Static[tuple[*T_]]) -> tuple[Args2[*T_], tuple[*T_]]:
+        return Args2(), args
+
+
+def test1() -> None:
+    assert Args2.make((1, 2, 3))[1] == (1, 2, 3)
