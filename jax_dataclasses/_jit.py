a45238cdd9cf0e677d2f3971fde71d4028c237d7
brentyi <yibrenth@gmail.com>
2025-12-19 09:24:32 -0800

    Remove `abstracted_axes`; very unlikely anyone is using it

--- jax_dataclasses/_jit.py
+++ jax_dataclasses/_jit.py
@@ -9,7 +9,6 @@ from ._get_type_hints import get_type_hints_partial
 
 CallableType = TypeVar("CallableType", bound=Callable)
 
-_JIT_PARAMS = set(inspect.signature(jax.jit).parameters)
 
 @overload
 def jit(
@@ -20,7 +19,6 @@ def jit(
     donate_argnums: Union[int, Sequence[int]] = (),
     inline: bool = False,
     keep_unused: bool = False,
-    abstracted_axes: Optional[Any] = None,
 ) -> CallableType: ...
 
 
@@ -33,7 +31,6 @@ def jit(
     donate_argnums: Union[int, Sequence[int]] = (),
     inline: bool = False,
     keep_unused: bool = False,
-    abstracted_axes: Optional[Any] = None,
 ) -> Callable[[CallableType], CallableType]: ...
 
 
@@ -45,7 +42,6 @@ def jit(
     donate_argnums: Union[int, Sequence[int]] = (),
     inline: bool = False,
     keep_unused: bool = False,
-    abstracted_axes: Optional[Any] = None,
 ) -> Union[CallableType, Callable[[CallableType], CallableType]]:
     """Light wrapper around `jax.jit`, with usability and type checking improvements.
 
@@ -87,16 +83,6 @@ def jit(
             inline=inline,
             keep_unused=keep_unused,
         )
-
-        if "abstracted_axes" in _JIT_PARAMS:
-            jit_kwargs["abstracted_axes"] = abstracted_axes
-        else:
-            if abstracted_axes is not None:
-                raise TypeError(
-                    "This JAX version does not support jax.jit(..., abstracted_axes=...). "
-                    "If you need shape polymorphism, use jax.export with symbolic shapes instead."
-                )
-
         return cast(CallableType, jax.jit(fun, **jit_kwargs))
 
     if fun is None:
